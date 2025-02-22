95063b7903925f0dada91477fe755d5b40c44077
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2025-02-22 11:41:07 +0100

    Update TheStuntman.ps1
    updated

--- public/TheStuntman.ps1
+++ public/TheStuntman.ps1
@@ -6,54 +6,110 @@ function New-Stuntman {
     param(
         [Parameter(Mandatory)]
         [ArgumentCompletionsSupportedLocales()]
-        $Locale
+        $Locale,
+
+        [Parameter()]
+        [string]
+        $CompanyName = 'Enyoi',
+
+        [Parameter()]
+        [int]
+        $Amount = 1,
+
+        [Parameter()]
+        [Switch]
+        $GenerateContracts,
+
+        [Parameter()]
+        [int]
+        $MaxContractsPerEmployee = 2
     )
 
     try {
-        $faker = [Bogus.Faker]::new($Locale)
-        $random = [Bogus.Randomizer]::new()
+        $persons = [System.Collections.Generic.List[Object]]::new()
+        $departmentManagers = @{}
 
-        $person = [PSCustomObject]@{
-            UserId    = $random.Int(1, 100000)
-            UserName = $faker.Person.UserName
-            Avatar   = $faker.Person.Avatar
-            FirstName = $faker.Person.FirstName
-            LastName  = $faker.Person.LastName
-            FullName  = $faker.Person.FullName
-            Gender    = $faker.Person.Gender.ToString()
-            Address = [PSCustomObject]@{
-                ZipCode          = $faker.Person.Address.ZipCode
-                StreetName       = $faker.Person.Address.Street
-                City             = $faker.Person.Address.City
-                Country          = $Locale
-                State            = $faker.Person.Address.State
-                Lng              = $faker.Person.Address.Geo.Lng
-                Lat              = $faker.Person.Address.Geo.Lat
+        for ($i = 0; $i -lt $Amount; $i++) {
+            $companyName = $CompanyName
+            $faker = [Bogus.Faker]::new($Locale)
+            $random = [Bogus.Randomizer]::new()
+            $contracts = [System.Collections.Generic.List[Object]]::new()
+            $firstName = $faker.Person.FirstName
+            $lastName = $faker.Person.LastName
+            
+            if ($Locale -eq 'nl') {
+                $prefixes = @('Van', 'De', 'Der', 'Den', 'Du', 'Ten', 'Ter', 'Vanden', 'Van der', 'Van den')
+                if ((Get-Random -Minimum 1 -Maximum 6) -eq 1) {
+                    $prefix = $prefixes | Get-Random
+                } else {
+                    $prefix = $null
+                }
             }
-            Contact = [PSCustomObject]@{
-                Phone = $faker.Person.Phone
-                Email = $faker.Person.Email
-            }
-            Contract = New-StuntmanContract -Faker $faker
 
+            $lastName = 'Doe'
+        
+            $person = [PSCustomObject]@{
+                firstName     = $firstName
+                lastName      = if ($prefix) { "$prefix $lastName"} else { $lastName }
+                fullName      = if ($prefix) { "$firstName $prefix $lastName" } else { "$firstName $lastName" }
+                initials      = ("{0}.{1}." -f $firstName[0], $lastName[0])
+                employeeId    = "$($random.Int(100000, 999999))"
+                prefix        = $prefix
+                userName      = ("{0}.{1}" -f $firstName, $lastName).ToLower()
+                emailAddress  = ("{0}.{1}@{2}" -f $firstName[0], $lastName, $companyName).ToLower()
+                companyName   = $companyName
+                contracts     = $contracts
+            }
+
+            if ($GenerateContracts) {
+                $contractsPerPerson = Get-Random -Minimum 1 -Maximum $MaxContractsPerEmployee
+
+                for ($j = 0; $j -lt $contractsPerPerson; $j++) {
+                    $startDate = (Get-Date).AddDays(- (Get-Random -Minimum 0 -Maximum (5 * 365))).AddDays((Get-Random -Minimum 0 -Maximum 14))
+                    $endDate = $startDate.AddDays((Get-Random -Minimum 0 -Maximum (5 * 365)))
+                    
+                    $department = [Department]::GetValues([Department]) | ForEach-Object { $_.ToString() } | Get-Random
+                    $manager = $false
+                    if (-not $departmentManagers.ContainsKey($department)) {
+                        $manager = $true
+                        $departmentManagers[$department] = $true
+                    }
+
+                    $contract = [PSCustomObject]@{
+                        employeeId   = $person.EmployeeId
+                        startDate    = $startDate
+                        endDate      = $endDate
+                        hoursPerWeek = (Get-Random -InputObject @(8, 16, 20, 24, 28, 32, 36, 40))
+                        title        = [JobTitle]::GetValues([JobTitle]) | ForEach-Object { $_.ToString() } | Get-Random
+                        department   = $department
+                        costCenter   = [CostCenter]::GetValues([CostCenter]) | ForEach-Object { $_.ToString() } | Get-Random
+                        manager      = $manager
+                    }
+                    $contracts.Add($contract)
+                }
+            }
+            $persons.Add($person)
+            $previouslyCreated.Add($person)
         }
-        $previouslyCreated.Add($person)
-        Write-Output $person
+        foreach ($p in $persons){
+            Write-Output $p
+        }
     } catch {
         $PSCmdlet.ThrowTerminatingError($_)
     }
 }
 
+
 function Get-Stuntmen {
     [CmdletBinding()]
     param(
         [Parameter()]
-        [Int]
-        $UserId
+        [string]
+        $EmployeeId
     )
     try {
-        if ($UserId){
-            $previouslyCreated | Where-Object { $_.UserId -eq $UserId }
+        if ($EmployeeId){
+            Write-Output $previouslyCreated | Where-Object { $_.employeeId -eq $EmployeeId }
         } else {
             $previouslyCreated
         }
@@ -127,38 +183,12 @@ function New-StuntDataGenerationObject {
 #endregion public functions
 
 #region private functions and helper classes
-function New-StuntmanContract {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [Bogus.Faker]
-        $Faker
-    )
-
-    try {
-        [PSCustomObject]@{
-            StartDate    = $Faker.Date.Past(5)
-            EndDate      = $Faker.Date.Future(5)
-            HoursPerWeek = $Faker.PickRandom(8, 16, 20, 32, 40)
-            Department   = $Faker.PickRandom([Department]::new())
-            JobTitle     = $Faker.Name.JobTitle()
-            JobArea      = $Faker.Name.JobArea()
-            JobType      = $Faker.Name.JobType()
-            CompanyName  = $Faker.Person.Company.Name
-            CatchPhrase  = $Faker.Person.Company.CatchPhrase
-            Bs           = $Faker.Person.Company.Bs
-        }
-    } catch {
-        $PSCmdlet.ThrowTerminatingError($_)
-    }
-}
-
 class ArgumentCompletionsSupportedLocales: System.Management.Automation.ArgumentCompleterAttribute {
     ArgumentCompletionsSupportedLocales() : base({
         [SupportedLocales] | Get-Member -Static -Type Properties | Select-Object -ExpandProperty Name
     }) { }
 }
-#endregion priave functions and helper classes
+#endregion private functions and helper classes
 
 #region enums
 enum LoremDataSet {
@@ -261,6 +291,7 @@ enum SystemDataGenerationType {
     ApplePushToken
     BlackBerryPin
 }
+
 enum Department {
     Engineering
     HR
@@ -275,5 +306,92 @@ enum Department {
     Sales
     Recruitment
     Management
+}
+
+enum JobTitle {
+    Developer
+    Manager
+    Consultant
+    Engineer
+    Analyst
+    Architect
+    Administrator
+    Technician
+    Designer
+    Coordinator
+    Director
+    Strategist
+    Supervisor
+    Specialist
+    Officer
+    Executive
+    Operator
+    Planner
+    Researcher
+    Trainer
+    Advisor
+    Representative
+    Inspector
+    Producer
+    Controller
+    Facilitator
+    AnalystSenior
+    EngineerSenior
+    DeveloperLead
+    ConsultantSenior
+    ManagerSenior
+    EngineerPrincipal
+    DirectorIT
+    DirectorHR
+    DirectorFinance
+    DirectorSales
+    DirectorMarketing
+    ChiefTechnologyOfficer
+    ChiefOperatingOfficer
+    ChiefFinancialOfficer
+    ChiefExecutiveOfficer
+}
+
+enum CostCenter {
+    CC1001
+    CC1002
+    CC1003
+    CC1004
+    CC1005
+    CC1006
+    CC1007
+    CC1008
+    CC1009
+    CC1010
+    CC1011
+    CC1012
+    CC1013
+    CC1014
+    CC1015
+    CC1016
+    CC1017
+    CC1018
+    CC1019
+    CC1020
+    CC1021
+    CC1022
+    CC1023
+    CC1024
+    CC1025
+    CC1026
+    CC1027
+    CC1028
+    CC1029
+    CC1030
+    CC1031
+    CC1032
+    CC1033
+    CC1034
+    CC1035
+    CC1036
+    CC1037
+    CC1038
+    CC1039
+    CC1040
 }
 #endregion enums
